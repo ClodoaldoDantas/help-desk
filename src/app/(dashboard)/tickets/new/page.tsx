5927b4139612c8f46a653b9a293972bdc91a4b3c
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-26 22:28:16 -0300

    add label and input components

--- src/app/(dashboard)/tickets/new/page.tsx
+++ src/app/(dashboard)/tickets/new/page.tsx
@@ -1,9 +1,19 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export default function NewTicketPage() {
   return (
     <>
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Novo Chamado</h1>
       </div>
+
+      <form>
+        <div className="grid w-full max-w-xl items-center gap-2">
+          <Label htmlFor="patrimony">Número do Patrimônio</Label>
+          <Input type="text" id="patrimony" />
+        </div>
+      </form>
     </>
   )
 }
