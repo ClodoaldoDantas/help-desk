61113b4b124fb43bd3d9e917b44a0b5f314fa91f
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-29 21:30:36 -0300

    Add @prisma/client and prisma dependencies

---
+++ prisma/migrations/20240730002946_create_users/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "clerk_user_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_clerk_user_id_key" ON "users"("clerk_user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
